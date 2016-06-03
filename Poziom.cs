3566ebcdd18a295c9b44fe2310e5ced037aab5a7
Gosiek180 <gosiek180@gmail.com>
2016-06-03 15:50:58 +0200

    Update Poziom.cs

--- Poziom.cs
+++ Poziom.cs
@@ -5,13 +5,21 @@ public class Poziom : MonoBehaviour
 {
     //stworzenie publicznej zmiennej TextMesh i przypisanie jej komponentu tekstowego
     public TextMesh textMesh;
+    //stworzenie publicznej zmiennej GameObject
+    public GameObject ok;
     //stworzenie publicznej zmiennej tekstowej o nazwie levelName
     public string levelName;
 
 	void Start () 
-    {
+    	{
         //nadanie komponentowi tekstu, tym tekstem jest wartość levelName
         textMesh.text = levelName;
+        
+        //zielony znacznik zostaje usunięty jeśli poziom nie zostanie ukończony
+	if(PlayerPrefs.GetInt(levelName+"_finished", 0) == 0)
+	{
+		Destroy(ok);
+	}
 	}
 
     void OnMouseDown()
